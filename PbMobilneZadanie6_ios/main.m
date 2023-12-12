8c615cdaf7f52a49aa4e33ad783647b6c58e80a0
Łukasz Kaliszewicz <48961301+lukaso1r@users.noreply.github.com>
2023-12-12 10:35:06 +0100

    a

---
+++ PbMobilneZadanie6_ios/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  PbMobilneZadanie6_ios
+//
+//  Created by student on 14/11/2023.
+//  Copyright © 2023 pb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
